2036d0d444f0dddc7f9c6be0b39f88c06bce4824
Üfeyra Bağcı <ufeyrabagci0@gmail.com>
2025-04-27 20:42:18 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import json
 
-st.set_page_config(page_title="📌 Proje Öneri Sistemi", page_icon="🚀")
+st.set_page_config(page_title="✨ Proje Öneri Sistemi", page_icon="🚀")
 st.title("✨ Proje Öneri Sistemi")
 
 # JSON'dan projeleri yükle
